1c4446183fea06da1ce33dd3d4bfe5a22e8cb13c
Andrey <andrei.positko.gb@gmail.com>
2021-08-04 02:23:31 +0300

    create handleInvestModalOpen/closeModal/handleInvest functions

--- src/components/LoansList/LoansList.jsx
+++ src/components/LoansList/LoansList.jsx
@@ -12,30 +12,71 @@ import * as Styled from './styled';
 
 const LoansList = () => {
     const [loans, setLoans] = useState(currentLoans.loans);
-    
-    const totalAmount = useMemo(
-    () => loans.reduce((acc, item) => {
-        const available = formatCurrencyToNumber(item.available);
+    const [isDisplayedModal, setIsDisplayedModal] = useState(false);
+    const [activeLoan, setActiveLoan] = useState(null);
 
-        return acc + available;
-    }, 0),
-    [loans],
+    const totalAmount = useMemo(
+        () => loans.reduce((acc, item) => {
+            const available = formatCurrencyToNumber(item.available);
+
+            return acc + available;
+        }, 0),
+        [loans],
     );
+
+    const handleInvestModalOpen = (id) => {
+        const loan = loans.find(item => item.id === id);
+
+        if (!loan) {
+            return;
+        }
+
+        setActiveLoan(loan);
+        setIsDisplayedModal(true)
+    }
+
+    const closeModal = () => {
+        setIsDisplayedModal(false)
+        setActiveLoan(null);
+    }
+
+    const handleInvest = (id, investValue) => {
+        const newLoans = [...loans];
+        const selectedIndex = newLoans.findIndex(item => item.id === id);
+
+        if (selectedIndex === -1) {
+            return;
+        }
+
+        const oldAvailable = newLoans[selectedIndex].available;
+
+        newLoans[selectedIndex].available = formatCurrencyToString(formatCurrencyToNumber(oldAvailable) - investValue);
+        newLoans[selectedIndex].invested = true;
+
+        setLoans(newLoans)
+        closeModal();
+    }
 
     return (
         <>
             <Styled.Wrapper>
                 <Styled.Title>current loans</Styled.Title>
                 {loans.map(loan => (
-                    <LoanItem key={loan.id} data={ loan }/>
+                    <LoanItem
+                        key={loan.id}
+                        data={loan}
+                        buttonOnClick={handleInvestModalOpen}
+                    />
                 ))}
-                
                 <Styled.TotalData>
                     Total amount available for investments: { formatCurrencyToString(totalAmount) }
                 </Styled.TotalData>
             </Styled.Wrapper>
-            <InvestModal>
-                <InvestContent/>
+            <InvestModal isOpenModal={isDisplayedModal} closeModal={closeModal}>
+                <InvestContent
+                    handleInvest={handleInvest}
+                    activeLoan={activeLoan}
+                />
             </InvestModal>
         </>
     )
